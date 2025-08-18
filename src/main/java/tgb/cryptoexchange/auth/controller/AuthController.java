85c3f75b90162eee9d3213022533a99aeff56102
barilo <e.barilo@isida.by>
2025-08-18 12:45:57 +0300

    оборачивание в ResponseEntity с http статусами

--- src/main/java/tgb/cryptoexchange/auth/controller/AuthController.java
+++ src/main/java/tgb/cryptoexchange/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tgb.cryptoexchange.auth.dto.UserCredentialsDTO;
+import tgb.cryptoexchange.auth.exception.AuthException;
 import tgb.cryptoexchange.auth.service.AuthService;
 import tgb.cryptoexchange.controller.ApiController;
 import tgb.cryptoexchange.web.ApiResponse;
@@ -22,30 +23,50 @@ public class AuthController extends ApiController {
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<String>> register(@RequestBody UserCredentialsDTO credentials) {
         if (!credentials.isValidForRegistration()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(
+                    ApiResponse.error(ApiResponse.Error.builder().message("Invalid data").build()),
+                    HttpStatus.BAD_REQUEST
+            );
         }
+        String token = authService.register(credentials.getUsername(), credentials.getPassword());
         return new ResponseEntity<>(
-                ApiResponse.success(
-                        authService.register(credentials.getUsername(), credentials.getPassword())
-                ),
+                ApiResponse.success(token),
                 HttpStatus.CREATED
         );
     }
 
     @PostMapping("/login")
-    public ApiResponse<String> login(@RequestBody UserCredentialsDTO credentials) {
-        return ApiResponse.success(
-                authService.login(credentials.getUsername(), credentials.getPassword())
+    public ResponseEntity<ApiResponse<String>> login(@RequestBody UserCredentialsDTO credentials) {
+        return new ResponseEntity<>(
+                ApiResponse.success(
+                        authService.login(credentials.getUsername(), credentials.getPassword())
+                ),
+                HttpStatus.CREATED
         );
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ApiResponse<Void> handleEntityNotFound(EntityNotFoundException e) {
-        return ApiResponse.error(
-                ApiResponse.Error.builder()
-                        .code(ApiResponse.Error.ErrorCode.ENTITY_NOT_FOUND)
-                        .message(e.getMessage())
-                        .build()
+    public ResponseEntity<ApiResponse<Void>> handleEntityNotFound(EntityNotFoundException e) {
+        return new ResponseEntity<>(
+                ApiResponse.error(
+                        ApiResponse.Error.builder()
+                                .code(ApiResponse.Error.ErrorCode.ENTITY_NOT_FOUND)
+                                .message(e.getMessage())
+                                .build()
+                ),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ApiResponse<Void>> handleAuthException(AuthException e) {
+        return new ResponseEntity<>(
+                ApiResponse.error(
+                        ApiResponse.Error.builder()
+                                .message(e.getMessage())
+                                .build()
+                ),
+                HttpStatus.FORBIDDEN
         );
     }
 }
