ce7da3cbaf7109da7eec21bc3383de8075cc9fbe
barilo <e.barilo@isida.by>
2025-08-18 16:27:26 +0300

    удалены неиспользуемые конструкторы

--- src/main/java/tgb/cryptoexchange/auth/exception/AuthException.java
+++ src/main/java/tgb/cryptoexchange/auth/exception/AuthException.java
@@ -4,15 +4,7 @@ package tgb.cryptoexchange.auth.exception;
  * Исключение сервиса авторизации
  */
 public class AuthException extends RuntimeException {
-
-    public AuthException() {
-    }
-
     public AuthException(String message) {
         super(message);
-    }
-
-    public AuthException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
