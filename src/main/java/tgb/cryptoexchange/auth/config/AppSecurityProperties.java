3dcda52be578934021d40c5262dd8d644c435665
barilo <barilo>
2026-02-16 13:54:24 +0300

    удалена @Data

--- src/main/java/tgb/cryptoexchange/auth/config/AppSecurityProperties.java
+++ src/main/java/tgb/cryptoexchange/auth/config/AppSecurityProperties.java
@@ -1,16 +1,19 @@
 package tgb.cryptoexchange.auth.config;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@NoArgsConstructor
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "app.security")
 public class AppSecurityProperties {
 
-    private List<String> ignoreUrls = new ArrayList<>();
+    private List<String> ignoreUrls;
 }
